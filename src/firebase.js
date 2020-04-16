0211ad88bc5731b97fcef6898a6726bcc0f05e88
parthvenkatesh <36329107+parthvenkatesh@users.noreply.github.com>
2020-04-16 12:42:25 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import {initializeApp} from 'firebase'
 const app = initializeApp({
-  apiKey: "AIzaSyBFPs4tNxs-AyrG_2AEv90GFRT-p7imwIk",
-  authDomain: "eloquent-anthem-238617.firebaseapp.com",
-  databaseURL: "https://eloquent-anthem-238617.firebaseio.com",
-  projectId: "eloquent-anthem-238617",
-  storageBucket: "eloquent-anthem-238617.appspot.com",
-  messagingSenderId: "693290027904",
-  appId: "1:693290027904:web:c30ad24120b0451525cf65"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  appId: "X:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 })
 
 export const db = app.firestore() 
